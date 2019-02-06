545123157302ea59dfcbcb4c917235162a1950f7
Eugene <eugene.shulga@dbbsoftware.com>
2019-02-06 17:35:12 +0200

    some commit

--- App.js
+++ App.js
@@ -8,8 +8,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { CameraKitGalleryView } from 'react-native-camera-kit';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -23,8 +23,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <CameraKitGalleryView title={'Press to scan'}/>
       </View>
     );
   }
